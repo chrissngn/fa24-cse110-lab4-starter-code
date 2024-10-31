7313d3f1f9d4348b7e17a6da0e05e94a9b966fb1
chrissngn <123513732+chrissngn@users.noreply.github.com>
2024-10-30 17:18:26 -0700

    update budget progress 1

--- client/src/utils/budget-utils.ts
+++ client/src/utils/budget-utils.ts
@@ -8,3 +8,16 @@ export const fetchBudget = async (): Promise<number> => {
     const jsonResponse = await response.json();
     return jsonResponse.data;
 };
+
+export const updateBudget = async (budget: number): Promise<number>=>{
+    const response = await fetch('${API_BASE_URL}/budget', {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({amount:budget}),
+    });
+    if(!response.ok){
+        throw new Error("Failed to upda")
+    }
+}
