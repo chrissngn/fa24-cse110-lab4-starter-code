b30e5af9b80e10fa599f7c1d2d2bb33e539332db
chrissngn <123513732+chrissngn@users.noreply.github.com>
2024-11-04 14:31:15 -0800

    exercise done!!

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -23,19 +23,23 @@ export async function createExpenseServer(req: Request, res: Response, db: Datab
 export async function deleteExpense(req: Request, res: Response, db: Database) {
     // TO DO: Implement deleteExpense function
     const{id} = req.params;
-
+    //if there is no id then output an error code 
     if (!id) {
         return res.status(400).json({ message: "Expense ID is required"});
     }
-
+    //try getting all the expenses 
     try{
         const expense = await db.get('SELECT * FROM expenses WHERE id = ?;',[id]);
+        //if there are no expenses throw an error 
         if(!expense){
             return res.status(404).json({message:"expense not found"});
         }
+        //otherwise delete the expense with the specified id 
         await db.run('DELETE FROM expenses WHERE id =?;',[id]);
+        //message to confirm successful delete 
         res.status(200).json({message:"Expense deleted successfully"});
     } catch(error){
+        //otherwise message to illustrate error 
         res.status(500).json({message: 'Error deleting: ${error.message}'});
     }
 
@@ -44,9 +48,11 @@ export async function deleteExpense(req: Request, res: Response, db: Database) {
 
 export async function getExpenses(req: Request, res: Response, db: Database) {
     try{
+        //try getting all expenses 
         const expenses = await db.all('SELECT * FROM expenses;');
         res.status(200).json({data:expenses});
     } catch(error){
+        //otherwise output error code 
         res.status(500).json({message: 'Error retrieving expenses:${error.message}'});
     }
 }
