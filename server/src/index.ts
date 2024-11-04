6fe9cab776cd3c62e5757fe4592ce3be4457c98e
chrissngn <123513732+chrissngn@users.noreply.github.com>
2024-11-04 13:56:14 -0800

    lab 6 so far

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,8 @@
-import { Request, Response } from "express";
-import { budget, expenses } from "./constants";
-import { createExpenseEndpoints } from "./expenses/expense-endpoints";
-import { deleteExpense } from "./expenses/expense-utils";
-import { ParamsDictionary } from "express-serve-static-core";
-import { ParsedQs } from "qs";
+import { Response } from "express";
 import { createBudgetEndpoints } from "./budget/budget-endpoints";
+import { createExpenseEndpoints } from "./expenses/expense-endpoints";
+import { budget } from "./constants";
+import initDB from "./createTable";
 
 const express = require("express");
 const cors = require("cors");
@@ -15,17 +13,22 @@ const port = 8080;
 app.use(cors());
 app.use(express.json());
 
-app.delete("/expenses/:id", (req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>) => deleteExpense(req, res, expenses));
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+ console.log(`Server running at http://localhost:${port}`);
 });
 
-// Root endpoint to get test if the server is running
-app.get("/", (req: Request, res: Response) => {
-  res.send({ "data": "Hello, TypeScript Express!" });
-  res.status(200);
-});
+// Initialize the database and start the server
+(async () => {
+ const db = await initDB();
 
-createExpenseEndpoints(app, expenses);
-createBudgetEndpoints(app, budget);
+ // Root endpoint to get test if the server is running
+ app.get("/", (res: Response) => {
+   res.send({ "data": "Hello, TypeScript Express!" });
+   res.status(200);
+ });
+
+ createExpenseEndpoints(app, db);
+
+ createBudgetEndpoints(app, budget);
+})();
